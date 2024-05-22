988abbfa5842a3b309a978353c426f5ed2b327cc
Fernando Cantanhede <105644403+fernanndogg@users.noreply.github.com>
2024-05-22 13:20:30 -0300

    Finalizado

--- paginas/menu.js
+++ paginas/menu.js
@@ -5,17 +5,17 @@ document.addEventListener("DOMContentLoaded", function() {
     const hamburger = document.getElementById("hamburger");
     const navHeader = document.getElementById("nav_header");
     const hamburgerIcon = document.getElementById("hamburger-icon");
-
+  
     hamburger.addEventListener("click", function() {
-
-        if (hamburgerIcon.getAttribute("data-lucide") === "menu") {
-            hamburgerIcon.setAttribute("data-lucide", "x");
-            navHeader.classList.add("active");
-        } else {
-            hamburgerIcon.setAttribute("data-lucide", "menu");
-            navHeader.classList.remove("active");
-        }
-        lucide.createIcons();
+      navHeader.classList.toggle("active"); 
+  
+      const currentIcon = hamburgerIcon.getAttribute("data-lucide");
+      if (currentIcon === "menu") {
+        hamburgerIcon.setAttribute("data-lucide", "x");
+      } else {
+        hamburgerIcon.setAttribute("data-lucide", "menu"); 
+      }
+      lucide.createIcons(); 
     });
-});
-
+  });
+  

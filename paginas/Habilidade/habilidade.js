af2410f778e6b1fecad448bf081869a50f481216
Fernando Cantanhede <105644403+fernanndogg@users.noreply.github.com>
2024-05-21 21:15:57 -0300

    Finalizado!

--- paginas/Habilidade/habilidade.js
+++ paginas/Habilidade/habilidade.js
@@ -13,3 +13,30 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+// typewriter
+document.addEventListener("DOMContentLoaded", function () {
+    const element = document.getElementById("typewriter");
+    const text = "Minhas Habilidades"; 
+    let index = 0;
+  
+    function typeWriter() {
+      if (index < text.length) {
+        if (text[index] === '<') {
+          while (text[index] !== '>') {
+            element.innerHTML += text[index];
+            index++;
+          }
+          element.innerHTML += text[index];
+        } else {
+          element.innerHTML += text[index];
+        }
+        index++;
+        setTimeout(typeWriter, 100);
+      }
+    }
+  
+    element.innerHTML = "";
+    typeWriter();
+  });
+  
